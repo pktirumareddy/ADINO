53228c7f5da24a1cfefa2873afd70ef4be1d2fe8
Praveen Kumar TIRUMAREDDY <pktirumareddy@gmail.com>
2024-04-28 07:41:25 +0530

    basic authentication and authorization added

--- src/main/java/com/eznite/adino/controller/ResponseEntityController.java
+++ src/main/java/com/eznite/adino/controller/ResponseEntityController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eznite.adino.dto.PayloadDTO;
@@ -18,6 +19,7 @@ import com.eznite.adino.util.CommonUtils;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/admin")
 public class ResponseEntityController {
 	
 	private static final Logger logger = LogManager.getLogger(ResponseEntityController.class);
