de79a5454676871de87139bacd91c601568f7667
Praveen Kumar TIRUMAREDDY <pktirumareddy@gmail.com>
2024-05-18 23:06:05 +0530

    session management is added but in RESTful APIs stateless authentication mechanisms like (JWT) are often preferred over traditional session management

--- src/main/java/com/eznite/adino/config/SecurityConfig.java
+++ src/main/java/com/eznite/adino/config/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.eznite.adino.config;
 
+import java.time.Instant;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,36 +23,41 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableScheduling
 public class SecurityConfig {
 
-	private static final String[] SWAGGERLIST = { "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**",
-			"/swagger-resources" };
+    private static final String[] SWAGGERLIST = { "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/swagger-resources" };
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeHttpRequests()
-				.requestMatchers(SWAGGERLIST).permitAll()
-				.requestMatchers("/admin/**").hasRole("ADMIN")
-				.requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-				.anyRequest().authenticated()
-				.and()
-				.formLogin().loginPage("/login").permitAll()
-				.and()
-				.logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll()
-				.and()
-				.exceptionHandling().accessDeniedPage("/403")
-				.and()
-				.httpBasic();
-		try {
-			return http.build();
-		} catch (Exception e) {
-			throw new RuntimeException("Error building SecurityFilterChain", e);
-		}
-	}
-	
-	@Bean
-    public UserDetailsService configureGlobal() throws Exception {
-		UserDetails user = User.builder()
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests()
+                .requestMatchers(SWAGGERLIST).permitAll()
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout").permitAll()
+                .and()
+                .exceptionHandling().accessDeniedPage("/403")
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                    .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                    .maximumSessions(1)
+                    .maxSessionsPreventsLogin(true)
+                    .sessionRegistry(sessionRegistry())
+                    .and()
+                .sessionFixation().migrateSession();
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
                 .username("user")
                 .password(passwordEncoder().encode("user@123"))
                 .roles("USER")
@@ -54,12 +67,38 @@ public class SecurityConfig {
                 .password(passwordEncoder().encode("admin@123"))
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user,admin);
+        return new InMemoryUserDetailsManager(user, admin);
     }
-	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-	    return new BCryptPasswordEncoder();
-	}
-	
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+
+    @Scheduled(fixedRate = 60000)
+    public void invalidateExpiredSessions() {
+        List<Object> principals = sessionRegistry().getAllPrincipals();
+        for (Object principal : principals) {
+            List<SessionInformation> sessions = sessionRegistry().getAllSessions(principal, false);
+            for (SessionInformation sessionInfo : sessions) {
+                if (isSessionExpired(sessionInfo)) {
+                    System.out.println("Removing expired session: " + sessionInfo.getSessionId());
+                    sessionRegistry().removeSessionInformation(sessionInfo.getSessionId());
+                }
+            }
+        }
+    }
+
+    private boolean isSessionExpired(SessionInformation sessionInfo) {
+        long sessionTimeoutInMillis = 1 * 60 * 1000; // 5 minutes
+        long lastRequestTimeInMillis = sessionInfo.getLastRequest().getTime();
+        long currentTimeInMillis = Instant.now().toEpochMilli();
+        return (currentTimeInMillis - lastRequestTimeInMillis) > sessionTimeoutInMillis;
+    }
 }
+
